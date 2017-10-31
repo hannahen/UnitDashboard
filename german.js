87d1276acced843844717e7966badb8cb9225f22
Hannah <h.e.newsome@gmail.com>
2017-10-30 19:57:37 -0600

    Create german.js

---
+++ german.js
@@ -0,0 +1,19 @@
+/*
+ * Everybody knows a little german, right? But remembering the correct articles is a tough job. 
+ * Write yourself a little helper, that returns the noun with the matching article:
+ *
+ *  each noun containing less than 2 vowels has the article "das"
+ *  each noun containing 2/3 vowels has the article "die"
+ *  each noun containing more than 3 vowels has the article "der"
+ *
+ * Caution: Vowels are "a,e,i,o,u". Umlaute (ä ö ü) are also being counted!
+ *
+ * (This Kata is a joke, there is no such grammar rule!)
+ */
+ 
+function derDieDas(wort) {
+  var vowels = wort.split(/[aeiouäöü]/i).length - 1
+  if (vowels < 2) return "das " + wort
+  if (vowels > 3) return "der " + wort
+  return "die " + wort
+}
